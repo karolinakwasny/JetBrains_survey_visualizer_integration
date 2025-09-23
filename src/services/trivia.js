d56e669e71a53ef75a6686beb84398765eaf9c30
karolinakwasny <kwasnykarolina98@gmail.com>
2025-09-23 08:18:06 +0200

    base64

--- src/services/trivia.js
+++ src/services/trivia.js
@@ -4,7 +4,7 @@ const BASE_URL = 'https://opentdb.com'
 
 const getQuestions = async (amount = 50) => {
   const res = await axios.get(
-    `${BASE_URL}/api.php?amount=${amount}&encode=url3986`
+    `${BASE_URL}/api.php?amount=${amount}&encode=base64`
   )
   return res.data.results
 }
