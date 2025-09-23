4a0b817a7c93c268b40bc5ffc94f5cab8909ee58
karolinakwasny <kwasnykarolina98@gmail.com>
2025-09-23 08:32:44 +0200

    better hero

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -9,9 +9,11 @@ const HeroSection = ({ chartsRef }) => {
         <h1 className="hero-text hero-text-large">Trivia Data Dashboard</h1>
         <p className="hero-paragraph">
           Explore trivia data from the Open Trivia Database. View categories,
-          question difficulties and distributions. Use the filter to select a
-          single category and visualize its data. This dashboard gives you an
-          interactive way to understand how trivia questions are organized.
+          question difficulties, and question distributions. Use the filters to
+          select a specific category and see how the questions are organized by
+          difficulty level (easy, medium, hard) and type (multiple choice or
+          true/false). This dashboard gives you an interactive way to understand
+          how trivia questions are organized.
         </p>
         <button className="hero-button" onClick={scrollToCharts}>
           Explore the data
