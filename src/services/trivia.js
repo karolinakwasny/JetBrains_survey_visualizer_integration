b3db409915b7a42fa447946ca975dd7c37846833
karolinakwasny <kwasnykarolina98@gmail.com>
2025-09-23 08:23:40 +0200

    fix api

--- src/services/trivia.js
+++ src/services/trivia.js
@@ -6,6 +6,7 @@ const getQuestions = async (amount = 50) => {
   const res = await axios.get(
     `${BASE_URL}/api.php?amount=${amount}&encode=base64`
   )
-  return res.data.results
+  return res.data
 }
+
 export default { getQuestions }
